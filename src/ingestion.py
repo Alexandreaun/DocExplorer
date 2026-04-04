017c01088e1812dbea1ee523ff362b078948293b
AlexandreAun <alexandreaun@gmail.com>
2026-04-04 16:45:24 -0300

    created ingestion of PDF document

---
+++ src/ingestion.py
@@ -0,0 +1,65 @@
+import os
+import sys
+from pathlib import Path
+from dotenv import load_dotenv
+
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_openai import OpenAIEmbeddings
+from langchain_core.documents import Document
+from langchain_postgres import PGVector
+
+load_dotenv()
+for k in ("OPENAI_API_KEY", "PGVECTOR_URL", "PGVECTOR_COLLECTION"):
+    if not os.getenv(k):
+        raise RuntimeError(f"Environment variable {k} is not set")
+
+
+def main():
+    current_dir = Path(__file__).resolve().parent
+    default_path = current_dir.parent / "data" / "criptografia.pdf"
+
+    if len(sys.argv) > 1:
+        pdf_path = Path(sys.argv[1]).resolve()
+    else:
+        pdf_path = default_path
+
+    if not pdf_path.exists():
+        raise SystemExit(f"File {pdf_path} does not exist")
+
+    print(f"🤖 Processando: {pdf_path.name}")
+
+    docs = PyPDFLoader(str(pdf_path)).load()
+
+    splits = RecursiveCharacterTextSplitter(
+        chunk_size=1000,
+        chunk_overlap=150,
+        add_start_index=False,
+    ).split_documents(docs)
+    if not splits:
+        raise SystemExit(0)
+
+    enriched = [
+        Document(
+            page_content=d.page_content.replace("\x00", ""),
+            metadata={k: v for k, v in d.metadata.items() if v not in ("", None)},
+        )
+        for d in splits
+    ]
+
+    ids = [f"doc-{i}" for i in range(len(enriched))]
+
+    embeddings = OpenAIEmbeddings(model=os.getenv("OPENAI_MODEL", "text-embedding-3-small"))
+
+    store = PGVector(
+        embeddings=embeddings,
+        collection_name=os.getenv("PGVECTOR_COLLECTION"),
+        connection=os.getenv("PGVECTOR_URL"),
+        use_jsonb=True,
+    )
+
+    store.add_documents(documents=enriched, ids=ids)
+    print(f"✅ Ingestão concluída com sucesso")
+
+if __name__ == "__main__":
+    main()
